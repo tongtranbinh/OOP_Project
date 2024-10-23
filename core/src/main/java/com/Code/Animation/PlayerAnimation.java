ac702bbb821cae48dd9597738bb1997accb3624f
tongtranbinh <tongtranbinh3008@gmail.com>
2024-10-24 05:58:30 +0700

    Add files via upload

---
+++ core/src/main/java/com/Code/Animation/PlayerAnimation.java
@@ -0,0 +1,46 @@
+package com.Code.Animation;
+
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.util.ArrayList;
+
+public class PlayerAnimation {
+    public ArrayList<Texture> up = new ArrayList<Texture>();
+    public ArrayList<Texture> down = new ArrayList<Texture>();
+    public ArrayList<Texture> right = new ArrayList<Texture>();
+    public ArrayList<Texture> left = new ArrayList<Texture>();
+    Texture Sprites;
+
+    public void CreatePlayerAnimation()
+    {
+        for(int i = 0; i <= 11 ; i++)
+        {
+            String path = "Player/Walk/Walk Down-split/imageonline/"+ i +"0.png";
+            Sprites = new Texture(path);
+            down.add(Sprites);
+        }
+        for(int i = 0; i <= 11 ; i++)
+        {
+            String path = "Player/Walk/Walk Up-split/imageonline/"+ i +"0.png";
+            Sprites = new Texture(path);
+            up.add(Sprites);
+        }
+        for(int i = 0; i <= 11 ; i++)
+        {
+            String path = "Player/Walk/Walk Right-split/imageonline/"+ i +"0.png";
+            Sprites = new Texture(path);
+            right.add(Sprites);
+        }
+        for(int i = 0; i <= 11 ; i++)
+        {
+            String path = "Player/Walk/Walk Left-split/imageonline/"+ i +"0.png";
+            Sprites = new Texture(path);
+            left.add(Sprites);
+        }
+    }
+}
+
