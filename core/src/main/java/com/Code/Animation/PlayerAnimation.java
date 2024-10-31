6b9d8f89631c4e595d03b4986f8716cb307759f8
TTBinh <tongtranbinh3008@gmail.com>
2024-10-31 09:02:44 +0700

    code

--- core/src/main/java/com/Code/Animation/PlayerAnimation.java
+++ core/src/main/java/com/Code/Animation/PlayerAnimation.java
@@ -1,101 +1,34 @@
 package com.Code.Animation;
 
 
+import com.Code.Entity.Player;
+import com.Code.Image.PlayerImage;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
 import java.util.ArrayList;
 
 public class PlayerAnimation {
-    public ArrayList<Texture> up = new ArrayList<Texture>();
-    public ArrayList<Texture> down = new ArrayList<Texture>();
-    public ArrayList<Texture> right = new ArrayList<Texture>();
-    public ArrayList<Texture> left = new ArrayList<Texture>();
-    public ArrayList<Texture> SlashUp = new ArrayList<Texture>();
-    public ArrayList<Texture> SlashDown = new ArrayList<Texture>();
-    public ArrayList<Texture> SlashRight = new ArrayList<Texture>();
-    public ArrayList<Texture> SlashLeft = new ArrayList<Texture>();
-    public ArrayList<Texture> SwordUp = new ArrayList<Texture>();
-    public ArrayList<Texture> SwordDown = new ArrayList<Texture>();
-    public ArrayList<Texture> SwordRight = new ArrayList<Texture>();
-    public ArrayList<Texture> SwordLeft = new ArrayList<Texture>();
-
-    Texture Sprites;
-
-    public void CreatePlayerAnimation()
-    {
-        for(int i = 0; i <= 11 ; i++)
-        {
-            String path = "Player/Walk/Walk Down-split/imageonline/"+ i +"0.png";
-            Sprites = new Texture(path);
-            down.add(Sprites);
-        }
-        for(int i = 0; i <= 11 ; i++)
-        {
-            String path = "Player/Walk/Walk Up-split/imageonline/"+ i +"0.png";
-            Sprites = new Texture(path);
-            up.add(Sprites);
-        }
-        for(int i = 0; i <= 11 ; i++)
-        {
-            String path = "Player/Walk/Walk Right-split/imageonline/"+ i +"0.png";
-            Sprites = new Texture(path);
-            right.add(Sprites);
-        }
-        for(int i = 0; i <= 11 ; i++)
-        {
-            String path = "Player/Walk/Walk Left-split/imageonline/"+ i +"0.png";
-            Sprites = new Texture(path);
-            left.add(Sprites);
-        }
-
-        for(int i = 0; i <= 4 ; i++)
-        {
-            String path = "Player/Attack/Slash Up-split/imageonline/" + i + "0.png";
-            Sprites = new Texture(path);
-            SlashUp.add(Sprites);
-        }
-        for(int i = 0; i <= 4 ; i++)
-        {
-            String path = "Player/Attack/Slash Left-split/imageonline/" + i + "0.png";
-            Sprites = new Texture(path);
-            SlashLeft.add(Sprites);
-        }
-        for(int i = 0; i <= 4 ; i++)
-        {
-            String path = "Player/Attack/Slash Right-split/imageonline/" + i + "0.png";
-            Sprites = new Texture(path);
-            SlashRight.add(Sprites);
-        }
-
-        for(int i = 0; i <= 4 ; i++)
-        {
-            String path = "Player/Attack/Sword Down-split/imageonline/" + i + "0.png";
-            Sprites = new Texture(path);
-            SwordDown.add(Sprites);
-        }
-        for(int i = 0; i <= 4 ; i++)
-        {
-            String path = "Player/Attack/Sword Up-split/imageonline/" + i + "0.png";
-            Sprites = new Texture(path);
-            SwordUp.add(Sprites);
-        }
-        for(int i = 0; i <= 4 ; i++)
-        {
-            String path = "Player/Attack/Sword Left-split/imageonline/" + i + "0.png";
-            Sprites = new Texture(path);
-            SwordLeft.add(Sprites);
-        }
-        for(int i = 0; i <= 4 ; i++)
-        {
-            String path = "Player/Attack/Sword Right-split/imageonline/" + i + "0.png";
-            Sprites = new Texture(path);
-            SwordRight.add(Sprites);
-        }
+    PlayerImage playerImage = new PlayerImage();
+    Player player = new Player(0,0);
+    public Texture Sprites = playerImage.up.get(0);
 
 
+    public void CreatePlayerAnimation(){
+
+        if(player.direction == 0){
+            Sprites = playerImage.up.get(0);
+        }
+        if(player.direction == 1){
+            Sprites = playerImage.left.get(0);
+        }
+        if(player.direction == 2){
+            Sprites = playerImage.down.get(0);
+        }
+        if(player.direction == 3){
+            Sprites = playerImage.right.get(0);
+        }
     }
+
 }
 
---
+++ core/src/main/java/com/Code/Screens/PlayScreen.java
@@ -0,0 +1,60 @@
+package com.Code.Screens;
+
+import com.Code.Animation.Animation;
+import com.Code.Animation.PlayerAnimation;
+import com.Code.Main;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+
+public class PlayScreen implements Screen {
+    Main game;
+    Animation animation;
+    Texture image = new Texture("libgdx.png");
+    PlayerAnimation playerAnimation = new PlayerAnimation();
+
+    public PlayScreen(Main game){
+        animation = new Animation();
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(animation.playerAnimation.Sprites,0,0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
