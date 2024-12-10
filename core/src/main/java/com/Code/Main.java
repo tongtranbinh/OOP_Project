005dd5b8dbda2cb870a3583ac0b985cfcfe95afb
HKhanh <hkhanhp19@gmail.com>
2024-12-10 09:56:22 +0700

    jjj

--- core/src/main/java/com/Code/Main.java
+++ core/src/main/java/com/Code/Main.java
@@ -5,6 +5,7 @@ import com.Code.Controller.KeyHandler;
 import com.Code.Entity.ECSEngine;
 import com.Code.Map.MapMangager;
 import com.Code.Screens.PlayScreen;
+import com.Code.Screens.LoadingScreen;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -13,15 +14,18 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.assets.AssetManager;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
+    public static final int V_WIDTH = 400;
+    public static final int V_HEIGHT = 208;
 
-    public static final float PPM = 1/16f;
+    public static final float PPM = 1 / 16f;
     public static final float MAX_STEP_TIME = 1/60f;
 
     public float BaseSize = 16 * PPM;
-    public int ScreenWidth = 800,ScreenHeight = 450;
+    public int ScreenWidth = 800, ScreenHeight = 450;
     public SpriteBatch batch;
     public World world;
 
@@ -31,6 +35,7 @@ public class Main extends Game {
     public MapMangager mapMangager;
     public WorldContactListener worldContactListener;
 
+    private AssetManager assetManager; // AssetManager để quản lý tài nguyên
 
     public static BodyDef bodyDef = new BodyDef();
     public static FixtureDef fixtureDef = new FixtureDef();
@@ -39,9 +44,11 @@ public class Main extends Game {
     public void create() {
         batch = new SpriteBatch();
 
+        assetManager = new AssetManager(); // Khởi tạo AssetManager
+
         keyHandler = new KeyHandler();
         Camera = new OrthographicCamera();
-        world = new World(new Vector2(0,0), true);
+        world = new World(new Vector2(0, 0), true);
 
         ecsEngine = new ECSEngine(this);
         mapMangager = new MapMangager(this);
@@ -49,13 +56,19 @@ public class Main extends Game {
         worldContactListener = new WorldContactListener(this);
         world.setContactListener(worldContactListener);
 
-        setScreen(new PlayScreen(this));
+        setScreen(new LoadingScreen(this)); // Chuyển tới màn hình loading khi game bắt đầu
         Gdx.input.setInputProcessor(keyHandler);
     }
 
 
 
-    public static void resetBox2D(){
+    // Hàm để lấy AssetManager
+    public AssetManager getAssetManager() {
+        return assetManager;
+    }
+
+
+    public static void resetBox2D() {
         bodyDef.type = BodyDef.BodyType.StaticBody;
         bodyDef.fixedRotation = false;
 
